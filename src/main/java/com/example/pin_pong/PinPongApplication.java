b4f46ebae80f78a14e2d42a5b4891408db8472a1
KimJinYeongZ <cheche903@naver.com>
2024-07-12 14:31:47 +0900

    first commit

---
+++ src/main/java/com/example/pin_pong/PinPongApplication.java
@@ -0,0 +1,13 @@
+package com.example.pin_pong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinPongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinPongApplication.class, args);
+	}
+
+}
